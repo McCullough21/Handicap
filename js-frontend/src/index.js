bdb79f64b4b8c9854226d3ffeb4ee7ed6393d2dd
github username <github email address>
2020-03-24 10:06:39 -0400

    login form

--- js-frontend/src/index.js
+++ js-frontend/src/index.js
@@ -2,3 +2,26 @@
 // render html with user specific info
 // gives option to add a new score /// which also gives selection of courses to choose from 
 // calculates handicap based on 8 most recent scores
+document.addEventListener("DOMContentLoaded", () => {
+    
+  });
+
+
+  
+
+//   function test() {
+//     let form = document.getElementById("create-task-form")
+//     form.addEventListener("submit", function(event) {
+//       event.preventDefault()
+//       let text = document.getElementById("fname")
+//       let h3 = document.createElement("h3")
+//       h3.innerText = text.value
+//       let main = document.querySelector("h2")
+//       main.appendChild(h3)
+//     })
+    
+//   }
+      
+  
+
+  
